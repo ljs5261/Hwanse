15f6504f85ec99e4f044779ad603323e075789e9
이진수 <ljs5261@gmail.com>
2018-11-12 16:35:49 +0900

    늑대 왼쪽,오른쪽 이동

--- wolf.py
+++ wolf.py
@@ -13,10 +13,12 @@ class IdleState:
     def do(wolf):
         if wolf.move_count < 30:
             wolf.frame = (wolf.frame + 1) % 5
+            wolf.dir = 0
             wolf.x -= (wolf.velocity * game_framework.frame_time)
             wolf.move_count += 1
         if wolf.move_count >= 30:
             wolf.frame = (wolf.frame + 1) % 5
+            wolf.dir = 1
             wolf.x += (wolf.velocity * game_framework.frame_time)
             wolf.move_count += 1
         if wolf.move_count == 60:
@@ -24,7 +26,10 @@ class IdleState:
 
     @staticmethod
     def draw(wolf):
-        wolf.image.clip_draw(wolf.frame * 64, 0, 64, 40, wolf.x, wolf.y)
+        if wolf.dir == 0:
+            wolf.image.clip_draw(wolf.frame * 64, 0, 64, 40, wolf.x, wolf.y)
+        else:
+            wolf.image.clip_draw(wolf.frame * 64, 40, 64, 40, wolf.x, wolf.y)
 
 
 class WolfScrollState:
@@ -32,10 +37,12 @@ class WolfScrollState:
     def do(wolf):
         if wolf.move_count < 30:
             wolf.frame = (wolf.frame + 1) % 5
+            wolf.dir = 0
             wolf.x -= 2 * (wolf.velocity * game_framework.frame_time)
             wolf.move_count += 1
         if wolf.move_count >= 30:
             wolf.frame = (wolf.frame + 1) % 5
+            wolf.dir = 1
             wolf.x += 0
             wolf.move_count += 1
         if wolf.move_count == 60:
@@ -43,18 +50,22 @@ class WolfScrollState:
 
     @staticmethod
     def draw(wolf):
-        wolf.image.clip_draw(wolf.frame * 64, 0, 64, 40, wolf.x, wolf.y)
+        if wolf.dir == 0:
+            wolf.image.clip_draw(wolf.frame * 64, 0, 64, 40, wolf.x, wolf.y)
+        else:
+            wolf.image.clip_draw(wolf.frame * 64, 40, 64, 40, wolf.x, wolf.y)
 
 
 class Wolf:
 
     def __init__(self):
         self.x, self.y = 1400, 66
-        self.image = load_image('./Resource/wolf1.png')
+        self.image = load_image('./Resource/wolf.png')
         self.frame = 0
         self.velocity = RUN_SPEED_PPS
         self.move_count = 0
         self.cur_state = IdleState
+        self.dir = 0
 
     def update(self):
         self.cur_state.do(self)
