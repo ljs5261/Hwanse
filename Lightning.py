a762698fb57a56dcc6ac9f265f730ad4a4db4218
이진수 <ljs5261@gmail.com>
2018-12-02 16:38:12 +0900

    번개와 아타호 충돌처리 완료;

--- Lightning.py
+++ Lightning.py
@@ -7,7 +7,7 @@ PIXEL_PER_METER = (100.0 / 2.0)     # pixel / meter
 RUN_SPEED_MPS = 3                 # meter / second
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)       # pixel / second, 75
 
-TIME_PER_ACTION = 0.7
+TIME_PER_ACTION = 0.3
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 4
 
@@ -21,19 +21,23 @@ class Lightning:
         self.image = load_image('./Resource/lightning.png')
         self.frame = 0
         self.velocity = RUN_SPEED_PPS
+        self.timer = 0
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 100, 0, 100, 600, self.x, self.y)
+        if self.timer <= 3:
+            self.image.clip_draw(int(self.frame) * 100, 0, 100, 600, self.x, self.y)
         draw_rectangle(*self.get_bb())
 
     def update(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
         self.x -= (self.velocity * game_framework.frame_time)
-
+        self.timer += 1
+        if self.timer == 36:
+            self.timer = 0
         ataho = Stage2_state.get_ataho()
-        if Stage2_state.collide(self, ataho):
-            ataho.life -= 30
+        if self.timer <= 3 and Stage2_state.collide(self, ataho):
+            ataho.life -= 50
             print(ataho.life)
 
     def get_bb(self):
-        return self.x - 40, self.y - 40, self.x + 35, self.y + 40
+        return self.x - 20, self.y - 280, self.x + 20, self.y + 280
